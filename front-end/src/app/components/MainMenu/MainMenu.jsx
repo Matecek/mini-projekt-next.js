109ca04a3714e94a1b1f3ff706e87d8f619cbd79
Mateusz Wojtas <103265505+Matecek@users.noreply.github.com>
2025-08-05 10:04:17 +0200

    MainPage

--- front-end/src/app/components/MainMenu/MainMenu.jsx
+++ front-end/src/app/components/MainMenu/MainMenu.jsx
@@ -1,12 +1,21 @@
+"use client";
+import { usePathname } from "next/navigation";
 import styles from "./MainMenu.module.css";
 import { GENDERS } from "@/app/constants/productCategories";
 
 export function MainMenu() {
+    const pathname = usePathname();
+
     return (
         <ul className={styles.mainMenu}>
             {GENDERS.map(({ path, categoryName }) => (
                 <li key={path}>
-                    <a href={path}>{categoryName}</a>
+                    <a
+                        className={pathname === path ? "active" : ""}
+                        href={path}
+                    >
+                        {categoryName}
+                    </a>
                 </li>
             ))}
         </ul>
